d59c24669932e080c0f6209620be2b12bdd2c44f
Peter Leite <peterleite.dev@gmail.com>
2021-10-03 12:11:47 -0300

    added função para controles sumirem depois que o mouse sair do player

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
-let videoAnime = document.getElementById('videoAnime');
+let video = document.getElementById('video');
 
 const backward=()=>{
-    videoAnime.currentTime -= 15
+    video.currentTime -= 15
 }
 
 const forward=()=>{
-    videoAnime.currentTime += 15
+    video.currentTime += 15
 }
 
 const decrease=()=>{
-    videoAnime.playbackRate -= .1
+    video.playbackRate -= .1
 
 }
 
 const increase=()=>{
-    videoAnime.playbackRate += .1
+    video.playbackRate += .1
 
 }
 
 const play=()=>{
-    videoAnime.play()
+    video.classList.toggle('playing')
+    if(video.classList.contains('playing')){
+        video.pause()
+    } else {
+        video.play()
+    }
 }
 
 const stop=()=>{
-    videoAnime.pause()
-    videoAnime.currentTime = 0;
+    video.pause()
+    video.currentTime = 0;
 }
+
+// Controles sumirem
+let player = document.querySelector('.container')
+let controls = document.querySelector('.controls');
+
+player.addEventListener('mouseover', (event)=>{
+    controls.style.visibility = 'visible'
+    console.log('entrou');
+})
+
+controls.addEventListener('mouseleave', (event)=>{
+    console.log('saiu');
+    setTimeout(()=>{
+        controls.style.visibility = 'hidden'
+        // event.target.style.display = 'none';
+    }, 1000)
+})
+
